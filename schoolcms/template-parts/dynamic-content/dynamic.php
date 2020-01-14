6c426e681e4fb6117ca2478bc1d89babd7f223cd
Louis-IM <57900415+Louis-IM@users.noreply.github.com>
2020-01-14 15:31:00 +0000

    Update dynamic.php

--- schoolcms/template-parts/dynamic-content/dynamic.php
+++ schoolcms/template-parts/dynamic-content/dynamic.php
@@ -7,6 +7,7 @@
 <?php elseif(get_sub_field('divider')):?>
 <div class="<?php the_sub_field('divider') ?>"></div>	
 <?php else:?>
-
-	<h1><strong>Something went wrong. Please check your settings and template</strong></h1>
+	<?php if(is_user_logged_in()):?>
+		<h1><strong>Something went wrong. Please check your settings and template</strong></h1>
+	<?php endif;?>
 <?php endif;?>
