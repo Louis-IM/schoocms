69feb9ea568e533d98f52f47f22f4bd2c7fb7bb8
Louis-IM <57900415+Louis-IM@users.noreply.github.com>
2020-01-22 14:33:13 +0000

    Update content-staff.php

--- schoolcms/template-parts/content-staff.php
+++ schoolcms/template-parts/content-staff.php
@@ -13,6 +13,28 @@
 	<?php $raw_content = get_the_content(); 
 	?>
 		<div class="col">
+			<?php if(get_field('position') || get_field('qualifications') || get_field('email') ): ?>
+				<div class="featured-staff-metas staff-meta">
+					<?php if(get_field('position')): ?>
+						<div class="staff-meta">
+							<div class="meta-label">Position:</div>
+							<div class="meta-value"><?php the_field('position') ; ?></div>
+						</div>
+					<?php endif; ?>
+					<?php if(get_field('qualifications')): ?>
+						<div class="staff-meta">
+							<div class="meta-label">Qualifications:</div>
+							<div class="meta-value"><?php the_field('qualifications') ; ?></div>
+						</div>
+					<?php endif; ?>
+					<?php if(get_field('email')): ?>
+						<div class="staff-meta">
+							<div class="meta-label">Email:</div>
+							<div class="meta-value"><a href="mailto:<?php the_field('email') ; ?>" class="email-value"><?php the_field('email') ; ?></a></div>
+						</div>
+					<?php endif; ?>
+				</div>
+			<?php endif; ?>
 			<div class="row body-text">
 				<div class="<?php echo (strpos($raw_content,'[column_divider]')) ? 'col-lg-6' : 'col-lg-12' ; ?>">
 					<?php the_content(); ?>
