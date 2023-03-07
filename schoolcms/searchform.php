b820e6de83be2cbf394adf37ec8a4f175cfa17f2
Esme-IM <esme@innermedia.co.uk>
2023-03-07 15:54:06 +0000

    CWV updates
    Removed IDs and added aria labels to input and button elements for accessibility.

--- schoolcms/searchform.php
+++ schoolcms/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>" class="searchform clear">
     <label class="screen-reader-text" for="s"></label>
-        <input type="text" value="" name="s" id="s" class="searchInput" placeholder="Search"/>
-        <button type="submit" id="searchSubmit" value="Search" class="searchSubmit"><i class="fa fa-search"></i>
+        <input type="text" value="" name="s" class="searchInput s" placeholder="Search" aria-label="Search" />
+        <button type="submit" value="Search" class="searchSubmit" aria-label="Search"><i class="fa fa-search"></i>
 </button>
 </form>
